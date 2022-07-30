731dc35272b979a3104fb8f1f5d15b00bbcfad0f
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-07-30 14:00:00 -0500

    updated code to add colors and window dimensions

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 #import tkinter library, the * is used to import all build in functions and modules in the library and ttk
 # and ttk is used to use the package to style the program's properties and its look and feel
+from curses import window
 import tkinter
 from tkinter import *
 from tkinter import ttk
 
 #colors
+co0 = "#444466" #black
+co1 = "#feffff" #white
+co2 = "#6f9fbd" #blue
+co3 = "#403d3d" #ash
+
+window = Tk()
+window.title("Monpi's Pokedex!")
+window.geometry('550x510')
+window.resizable(width=False, height=False)
+window.configure(bg=co1)
+
+window.mainloop()

1717b3ddcf001bcbdb2f370cfd88b24cfe9053b7
santiago-mon-pi <santiago.monsalve1198@gmail.com>
2022-07-30 13:51:42 -0500

    added main file

---
+++ main.py
@@ -0,0 +1,7 @@
+#import tkinter library, the * is used to import all build in functions and modules in the library and ttk
+# and ttk is used to use the package to style the program's properties and its look and feel
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+#colors
